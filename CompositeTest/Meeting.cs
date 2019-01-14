52faf8bfa729c0a503b5b2a6efdadaab9daade50
Aram Farzin <AramFarzin@users.github.com>
2019-01-14 14:43:12 -0800

    CompositeTest

---
+++ CompositeTest/Meeting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CompositeTest
+{
+    internal class Meeting
+    {
+        public Meeting()
+        {
+            SubMeetings = new List<Meeting>();
+        }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public List<Meeting> SubMeetings { get; set; }
+
+        public void Add(Meeting meeting)
+        {
+            SubMeetings.Add(meeting);
+        }
+
+        public void Remove(Meeting meeting)
+        {
+            SubMeetings.Remove(meeting);
+        }
+        
+        public List<Meeting> GetSubMeetings()
+        {
+            return SubMeetings;
+        }
+    }
+}
---
+++ CompositeTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CompositeTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var scrumMeeting = new Meeting
+            {
+                Id = 1,
+                Name = "ScrumMeeting"
+            };
+
+            var dailyMeeting = new Meeting
+            {
+                Id = 2,
+                Name = "DailyMeeting"
+            };
+
+            var planningMeeting = new Meeting
+            {
+                Id = 3,
+                Name = "planningMeeting"
+            };
+            scrumMeeting.Add(dailyMeeting);
+            scrumMeeting.Add(planningMeeting);
+
+            foreach (var scrumMeetingSubMeeting in scrumMeeting.SubMeetings)
+                Console.WriteLine(scrumMeetingSubMeeting.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
